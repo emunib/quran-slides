afe458f1c57a9c1826ddf5778d65aab5b43e8201
Ehtasham <emunib1@gmail.com>
2020-11-01 11:41:54 -0600

    Remove custom scroll, move button

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,3 @@
-// $(document).ready(function () {
-//     // $("#sidebar").mCustomScrollbar({
-//     //     theme: "minimal"
-//     // });
-//
-//     $('#sidebar-close-button, #go-button, .overlay').on('click', function () {
-//         $('#sidebar').removeClass('active');
-//         $('.overlay').removeClass('active');
-//     });
-//
-//     $('#sidebar-open-button').on('click', function () {
-//         $('#sidebar').addClass('active');
-//         $('.overlay').addClass('active');
-//         $('.collapse.in').toggleClass('in');
-//         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-//     });
-// });
-
 let imgPath = "images/"
 let imgType = ".png"
 let r = [0, 97]
